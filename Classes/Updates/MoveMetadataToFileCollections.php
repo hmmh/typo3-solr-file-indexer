acb017fda44fc71bdc8e15d41230ff206cfdd218
Sascha Wilking <Sascha.Wilking@hmmh.de>
2023-08-14 09:20:21 +0200

    refactor update wizard task

--- Classes/Updates/MoveMetadataToFileCollections.php
+++ Classes/Updates/MoveMetadataToFileCollections.php
@@ -29,55 +29,43 @@ namespace HMMH\SolrFileIndexer\Updates;
 
 use HMMH\SolrFileIndexer\Resource\MetadataRepository;
 use HMMH\SolrFileIndexer\Utility\BaseUtility;
-use TYPO3\CMS\Core\Authentication\BackendUserAuthentication;
-use TYPO3\CMS\Core\Core\Bootstrap;
-use TYPO3\CMS\Core\Core\Environment;
-use TYPO3\CMS\Core\Core\SystemEnvironmentBuilder;
 use TYPO3\CMS\Core\Database\ConnectionPool;
-use TYPO3\CMS\Core\Http\ServerRequest;
+use TYPO3\CMS\Core\Exception\SiteNotFoundException;
 use TYPO3\CMS\Core\Site\SiteFinder;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Install\Updates\DatabaseUpdatedPrerequisite;
 use TYPO3\CMS\Install\Updates\UpgradeWizardInterface;
 use TYPO3\CMS\Core\Resource\ResourceFactory;
-use TYPO3\CMS\Core\DataHandling\DataHandler;
 
 class MoveMetadataToFileCollections implements UpgradeWizardInterface
 {
 
+    /**
+     * @return string
+     */
     public function getTitle(): string
     {
         return 'Create FileCollections from Metadata';
     }
 
+    /**
+     * @return string
+     */
     public function getDescription(): string
     {
         return 'Migrate enable_indexing from sys_file_metadata to sys_file_collection';
     }
 
+    /**
+     * @return bool
+     * @throws \Doctrine\DBAL\Exception
+     * @throws \TYPO3\CMS\Core\Resource\Exception\FileDoesNotExistException
+     */
     public function executeUpdate(): bool
     {
-        if (Environment::isCli() === false) {
-            $request = (new ServerRequest())->withAttribute('applicationType', SystemEnvironmentBuilder::REQUESTTYPE_BE);
-            Bootstrap::initializeBackendUser(BackendUserAuthentication::class, $request);
-            Bootstrap::initializeBackendAuthentication();
-            Bootstrap::initializeLanguageObject();
-        }
-
-        $success = true;
-
         $siteFinder = GeneralUtility::makeInstance(SiteFinder::class);
 
-        $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable(MetadataRepository::FILE_TABLE);
-        $where = [
-            $queryBuilder->expr()->neq('enable_indexing', $queryBuilder->createNamedParameter('')),
-            $queryBuilder->expr()->eq(BaseUtility::getMetadataLanguageField(), 0)
-        ];
-        $result = $queryBuilder->select('file', 'enable_indexing')
-            ->from(MetadataRepository::FILE_TABLE)
-            ->where(...$where)
-            ->executeQuery()
-            ->fetchAllAssociative();
+        $result = $this->getMetadataToTransform();
 
         $roots = [];
         foreach ($result as $metadata) {
@@ -87,64 +75,44 @@ class MoveMetadataToFileCollections implements UpgradeWizardInterface
             }
         }
 
+        $filesWithWrongRootpages = [];
+
         foreach ($roots as $rootPage => $fileUids) {
             try {
                 $site = $siteFinder->getSiteByRootPageId($rootPage);
+            } catch (SiteNotFoundException) {
+                $filesWithWrongRootpages[] = $fileUids;
+                continue;
+            }
+            $collection = $this->createNewFileCollection($rootPage);
 
-                $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable('sys_file_collection');
-                $queryBuilder->insert('sys_file_collection')
-                    ->values([
-                        'pid' => $site->getRootPageId(),
-                        'title' => 'autocreated-solr-file-indexer-' . (string)$rootPage,
-                        'type' => 'static',
-                        'use_for_solr' => (string)$rootPage,
-                        'tstamp' => time(),
-                        'crdate' => time()
-                    ])
-                    ->executeStatement();
+            $fileCounter = 1;
 
-                $collectionUid = $queryBuilder->getConnection()->lastInsertId();
+            foreach ($fileUids as $fileUid) {
+                $resourceFactory = GeneralUtility::makeInstance(ResourceFactory::class);
+                $fileObject = $resourceFactory->getFileObject($fileUid);
 
-                foreach ($fileUids as $fileUid) {
-                    $resourceFactory = GeneralUtility::makeInstance(ResourceFactory::class);
-                    $fileObject = $resourceFactory->getFileObject($fileUid);
+                $this->createFileReference($rootPage, $collection['uid'], $fileObject->getUid(), $fileCounter);
 
-                    $newId = 'NEW1234';
-                    $data = [];
-                    $data['sys_file_reference'][$newId] = [
-                        'table_local' => 'sys_file',
-                        'uid_local' => $fileObject->getUid(),
-                        'tablenames' => 'sys_file_collection',
-                        'uid_foreign' => $collectionUid,
-                        'fieldname' => 'assets',
-                        'pid' => $site->getRootPageId()
-                    ];
-                    $data['sys_file_collection'][$collectionUid] = [
-                        'files' => $newId
-                    ];
-                    /** @var DataHandler $dataHandler */
-                    $dataHandler = GeneralUtility::makeInstance(DataHandler::class);
-                    $dataHandler->start($data, []);
-                    $dataHandler->process_datamap();
+                $fileCounter++;
 
-                    $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable(MetadataRepository::FILE_TABLE);
-                    $queryBuilder->update(MetadataRepository::FILE_TABLE)
-                        ->where($queryBuilder->expr()->eq('file', $fileUid))
-                        ->set('enable_indexing', '')
-                        ->executeStatement();
-
-                    if (count($dataHandler->errorLog) !== 0) {
-                        $success = false;
-                    }
-                }
-            } catch (\Exception) {
-                return false;
+                $this->removeMetadataEntry($fileUid);
             }
         }
 
-        return $success;
+        foreach ($filesWithWrongRootpages as $fileUids) {
+            foreach ($fileUids as $fileUid) {
+                $this->removeMetadataEntry($fileUid);
+            }
+        }
+
+        return empty($this->getMetadataToTransform());
     }
 
+    /**
+     * @return bool
+     * @throws \Doctrine\DBAL\Exception
+     */
     public function updateNecessary(): bool
     {
         $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable(MetadataRepository::FILE_TABLE);
@@ -159,10 +127,105 @@ class MoveMetadataToFileCollections implements UpgradeWizardInterface
         return $result > 0;
     }
 
+    /**
+     * @return string[]
+     */
     public function getPrerequisites(): array
     {
         return [
             DatabaseUpdatedPrerequisite::class
         ];
+    }
+
+    /**
+     * @return \mixed[][]
+     * @throws \Doctrine\DBAL\Exception
+     */
+    private function getMetadataToTransform()
+    {
+        $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable(MetadataRepository::FILE_TABLE);
+        $where = [
+            $queryBuilder->expr()->neq('enable_indexing', $queryBuilder->createNamedParameter('')),
+            $queryBuilder->expr()->eq(BaseUtility::getMetadataLanguageField(), 0)
+        ];
+        return $queryBuilder->select('file', 'enable_indexing')
+            ->from(MetadataRepository::FILE_TABLE)
+            ->where(...$where)
+            ->executeQuery()
+            ->fetchAllAssociative();
+    }
+
+    /**
+     * @param int $rootPage
+     *
+     * @return false|mixed[]
+     * @throws \Doctrine\DBAL\Exception
+     */
+    private function createNewFileCollection(int $rootPage)
+    {
+        $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable('sys_file_collection');
+        $queryBuilder->insert('sys_file_collection')
+            ->values([
+                'pid' => $rootPage,
+                'title' => 'autocreated-solr-file-indexer-' . (string)$rootPage,
+                'type' => 'static',
+                'use_for_solr' => (string)$rootPage,
+                'tstamp' => time(),
+                'crdate' => time()
+            ])
+            ->executeStatement();
+
+        $collectionUid = $queryBuilder->getConnection()->lastInsertId();
+
+        $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable('sys_file_collection');
+        return $queryBuilder->select('*')
+            ->from('sys_file_collection')
+            ->where($queryBuilder->expr()->eq('uid', $collectionUid))
+            ->executeQuery()
+            ->fetchAssociative();
+    }
+
+    /**
+     * @param int $rootPage
+     * @param int $collectionUid
+     * @param int $fileUid
+     * @param int $fileCounter
+     *
+     * @return void
+     */
+    private function createFileReference(int $rootPage, int $collectionUid, int $fileUid, int $fileCounter): void
+    {
+        $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable('sys_file_reference');
+        $queryBuilder->insert('sys_file_reference')
+            ->values([
+                'uid_local' => $fileUid,
+                'tablenames' => 'sys_file_collection',
+                'uid_foreign' => $collectionUid,
+                'fieldname' => 'files',
+                'pid' => $rootPage,
+                'tstamp' => time(),
+                'crdate' => time()
+            ])
+            ->executeStatement();
+
+        $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable('sys_file_collection');
+        $queryBuilder->update('sys_file_collection')
+            ->where($queryBuilder->expr()->eq('uid', $collectionUid))
+            ->set('files', $fileCounter)
+            ->executeStatement();
+    }
+
+    /**
+     * @param int $fileUid
+     *
+     * @return void
+     */
+    private function removeMetadataEntry(int $fileUid): void
+    {
+        $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable(MetadataRepository::FILE_TABLE);
+        $queryBuilder->update(MetadataRepository::FILE_TABLE)
+            ->where($queryBuilder->expr()->eq('file', $fileUid))
+            ->set('enable_indexing', '')
+            ->executeStatement();
     }
 }
