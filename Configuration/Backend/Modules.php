8d1c1919a8ee074e75084181b73d7af90458f1fd
Sascha Wilking <Sascha.Wilking@hmmh.de>
2026-05-15 09:32:05 +0200

    fix backend module

--- Configuration/Backend/Modules.php
+++ Configuration/Backend/Modules.php
@@ -5,7 +5,7 @@ use HMMH\SolrFileIndexer\Controller\Backend\FileAdministrationController;
 return [
     'solr_file_indexer_fileadministration' => [
         'parent' => 'searchbackend',
-        'access' => 'user,group',
+        'access' => 'user',
         'path' => '/module/searchbackend/solr-file-indexer-file-administration',
         'iconIdentifier' => 'extensions-solr-file-indexer-module-file-admin',
         'labels' => 'LLL:EXT:solr_file_indexer/Resources/Private/Language/locallang_mod_fileadmin.xlf',
